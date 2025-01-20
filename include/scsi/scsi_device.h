6c1b510f2df3ce72d432ed9016dbda2eb2807a5b
Kai Mäkisara <Kai.Makisara@kolumbus.fi>
2025-01-20 21:49:23 +0200

    scsi: scsi_error: Add counters for New Media and Power On/Reset UNIT ATTENTIONs
    The purpose of the counters is to enable all ULDs attached to a
    device to find out that a New Media or/and Power On/Reset Unit
    Attentions has/have been set, even if another ULD catches the
    Unit Attention as response to a SCSI command.
    
    The ULDs can read the counters and see if the values have changed from
    the previous check.
    
    Signed-off-by: Kai Mäkisara <Kai.Makisara@kolumbus.fi>

--- include/scsi/scsi_device.h
+++ include/scsi/scsi_device.h
@@ -247,6 +247,9 @@ struct scsi_device {
 	unsigned int queue_stopped;	/* request queue is quiesced */
 	bool offline_already;		/* Device offline message logged */
 
+	u16 ua_new_media_ctr;		/* Counter for New Media UNIT ATTENTIONs */
+	u16 ua_por_ctr;			/* Counter for Power On / Reset UAs */
+
 	atomic_t disk_events_disable_depth; /* disable depth for disk events */
 
 	DECLARE_BITMAP(supported_events, SDEV_EVT_MAXBITS); /* supported events */
