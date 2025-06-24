9dcfee70992eafe66155efc2c0d141821595ea2c
Hannes Reinecke <hare@kernel.org>
2025-06-24 16:20:19 -0400

    qla2xxx: enable FPIN notification for NVMe
    Call 'nvme_fc_fpin_rcv()' to enable FPIN notifications for NVMe.
    
    Signed-off-by: Hannes Reinecke <hare@kernel.org>
    Reviewed-by: John Meneghini <jmeneghi@redhat.com>
    Tested-by: Muneendra Kumar <muneendra.kumar@broadcom.com>

--- drivers/scsi/qla2xxx/qla_isr.c
+++ drivers/scsi/qla2xxx/qla_isr.c
@@ -46,6 +46,9 @@ qla27xx_process_purex_fpin(struct scsi_qla_host *vha, struct purex_item *item)
 		       pkt, pkt_size);
 
 	fc_host_fpin_rcv(vha->host, pkt_size, (char *)pkt, 0);
+#if (IS_ENABLED(CONFIG_NVME_FC))
+	nvme_fc_fpin_rcv(vha->nvme_local_port, pkt_size, (char *)pkt);
+#endif
 }
 
 const char *const port_state_str[] = {
