3b83ff4df2187687cf291b16c45a3e2d5e23c23c
Gustavo A. R. Silva <gustavoars@kernel.org>
2025-07-30 16:54:52 -0600

    scsi: qla2xxx: Fix 2 memcpy field-spanning write issue
    purex_item.iocb is defined as a 64-element u8 array, but 64 is the
    minimum size and it can be allocated larger. This makes it a standard
    empty flex array.
    
    This was motivated by field-spanning write warnings during FPIN testing.
    
      >  kernel: memcpy: detected field-spanning write (size 60) of single
      >  field "((uint8_t *)fpin_pkt + buffer_copy_offset)"
      >  at drivers/scsi/qla2xxx/qla_isr.c:1221 (size 44)
    
    I removed the outer wrapper from the iocb flex array, so that it can be
    linked to `purex_item.size` with `__counted_by`.
    
    These changes remove the default minimum 64-byte allocation, requiring
    further changes.
    
      In `struct scsi_qla_host` the embedded `default_item` is now followed
      by `__default_item_iocb[QLA_DEFAULT_PAYLOAD_SIZE]` to reserve space
      that will be used as `default_item.iocb`. This is wrapped using the
      `TRAILING_OVERLAP()` macro helper, which effectively creates a union
      between flexible-array member `default_item.iocb` and
      `__default_item_iocb`.
    
      Since `struct pure_item` now contains a flexible-array member, the
      helper must be placed at the end of `struct scsi_qla_host` to prevent
      a `-Wflex-array-member-not-at-end` warning.
    
      `qla24xx_alloc_purex_item()` is adjusted to no longer expect the
      default minimum size to be part of `sizeof(struct purex_item)`,
      the entire flexible array size is added to the structure size for
      allocation.
    
    This also slightly changes the layout of the purex_item struct, as
    2-bytes of padding are added between `size` and `iocb`. The resulting
    size is the same, but iocb is shifted 2-bytes (the original `purex_item`
    structure was padded at the end, after the 64-byte defined array size).
    I don't think this is a problem.
    
    In qla_os.c:qla24xx_process_purex_rdp()
    
    To avoid a null pointer dereference the vha->default_item should be set
    to 0 last if the item pointer passed to the function matches.  Also use
    a local variable to avoid multiple de-referencing of the item.
    
    Tested-by: Bryan Gurney <bgurney@redhat.com>
    Co-developed-by: Chris Leech <cleech@redhat.com>
    Signed-off-by: Chris Leech <cleech@redhat.com>
    Signed-off-by: Gustavo A. R. Silva <gustavoars@kernel.org>

--- drivers/scsi/qla2xxx/qla_os.c
+++ drivers/scsi/qla2xxx/qla_os.c
@@ -6459,9 +6459,12 @@ dealloc:
 void
 qla24xx_free_purex_item(struct purex_item *item)
 {
-	if (item == &item->vha->default_item)
-		memset(&item->vha->default_item, 0, sizeof(struct purex_item));
-	else
+	scsi_qla_host_t *base_vha = item->vha;
+
+	if (item == &base_vha->default_item) {
+		memset(&base_vha->__default_item_iocb, 0, QLA_DEFAULT_PAYLOAD_SIZE);
+		memset(&base_vha->default_item, 0, sizeof(struct purex_item));
+	} else
 		kfree(item);
 }
 
