8bdf20b20f3ebecb176d0bdbc5eb439d95a71256
John Meneghini <jmeneghi@redhat.com>
2026-04-20 16:30:58 -0400

    nvmet: instrument nvmet_execute_cancel function
    Signed-off-by: John Meneghini <jmeneghi@redhat.com>

--- drivers/nvme/target/io-cmd-cancel.c
+++ drivers/nvme/target/io-cmd-cancel.c
@@ -17,11 +17,17 @@ void nvmet_execute_cancel(struct nvmet_req *req)
 	struct nvmet_req *treq;
 	u32 canceled = 0;
 
-	if (!nvmet_check_transfer_len(req, 0))
+
+	pr_info("nvmet: CANCEL command_id: 0x%x", req->cmd->cancel.command_id);
+
+	if (!nvmet_check_transfer_len(req, 0)) {
+		pr_info("nvmet: CANCEL invalid transfer length");
 		return;
+	}
 
 	sqid = le16_to_cpu(req->cmd->cancel.sqid);
 	if (sqid > ctrl->subsys->max_qid) {
+		pr_info("nvmet: CANCEL invalid sqid 0x%x", sqid);
 		ret = NVME_SC_INVALID_FIELD | NVME_STATUS_DNR;
 		goto exit;
 	}
@@ -29,12 +35,16 @@ void nvmet_execute_cancel(struct nvmet_req *req)
 	mult_cmds = req->cmd->cancel.action & NVME_CANCEL_ACTION_MUL_CMD;
 	cid  = req->cmd->cancel.cid;
 
+	pr_info("nvmet: CANCEL command_id 0x%x cid 0x%x mult_cmds %d",
+			req->cmd->cancel.command_id, cid, mult_cmds);
+
 	if (cid == req->cmd->cancel.command_id && !mult_cmds) {
 		/* If action is set to "single command" and cid is
 		 * set to the cid of this cancel command, then
 		 * the controller shall abort the command with
 		 * an "invalid cid" status code.
 		 */
+		pr_info("nvmet: CANCEL invalid cid 0x%x", cid);
 		ret = NVME_SC_INVALID_CID | NVME_STATUS_DNR;
 	} else if ((cid != 0xFFFF && mult_cmds) || sqid != sq->qid) {
 		/* if action is set to "multiple commands" and
@@ -44,6 +54,7 @@ void nvmet_execute_cancel(struct nvmet_req *req)
 		 * the queue to which the cancel command is submitted,
 		 * then abort the command with an "invalid field" status.
 		 */
+		pr_info("nvmet: CANCEL invalid field cid 0x%x sqid %d sq->qid %d", cid, sqid, sq->qid);
 		ret = NVME_SC_INVALID_FIELD | NVME_STATUS_DNR;
 	}
 
@@ -51,14 +62,14 @@ void nvmet_execute_cancel(struct nvmet_req *req)
 		treq = xa_load(&sq->outstanding_requests, cid);
 		if (treq) {
 			if (cancel_delayed_work(&treq->req_work)) {
-				pr_info("nvmet: CANCEL success: %d", cid);
+				pr_info("nvmet: CANCEL success: cid 0x%x", cid);
 				nvmet_req_complete(treq, NVME_SC_ABORT_REQ);
 				canceled += 1;
 			} else {
-				pr_info("nvmet: CANCEL failed: %d", cid);
+				pr_info("nvmet: CANCEL failed: cid 0x%x", cid);
 			}
 		} else {
-			pr_info("nvmet: CANCEL request not found: %d", cid);
+			pr_info("nvmet: CANCEL request not found: cid 0x%x", cid);
 		}
 	} else {
 		unsigned long ucid;
