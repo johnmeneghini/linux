0e1fcbb65b702755056008eac80a26ea81fcbf0a
Jesse Taube <jtaubepe@redhat.com>
2026-08-12 17:07:35 -0400

    nvme-multipath: queue-depth support for marginal paths

--- drivers/nvme/host/multipath.c
+++ drivers/nvme/host/multipath.c
@@ -464,50 +464,41 @@ out:
 static struct nvme_ns *nvme_queue_depth_path(struct nvme_ns_head *head)
 {
 	struct nvme_ns *best_opt = NULL, *best_nonopt = NULL, *ns;
-	int min_depth_opt = INT_MAX, min_depth_nonopt = INT_MAX;
-	bool opt_is_marginal = true, nonopt_is_marginal = true, marginal;
-
+	unsigned int min_depth_opt = UINT_MAX, min_depth_nonopt = UINT_MAX;
 	unsigned int depth;
+	bool need_marginal = nvme_all_paths_marginal(head);
 
 	list_for_each_entry_srcu(ns, &head->list, siblings,
 				 srcu_read_lock_held(&head->srcu)) {
 		if (nvme_path_is_disabled(ns))
 			continue;
 
+		/* Skip marginal paths unless we need to use them */
+		if (!need_marginal && nvme_ctrl_is_marginal(ns->ctrl))
+			continue;
+
 		depth = atomic_read(&ns->ctrl->nr_active);
-		marginal = nvme_ctrl_is_marginal(ns->ctrl);
 
 		switch (ns->ana_state) {
 		case NVME_ANA_OPTIMIZED:
-			if (is_best_distance(opt_is_marginal, marginal,
-					     min_depth_opt, depth)) {
+			if (depth < min_depth_opt) {
 				min_depth_opt = depth;
 				best_opt = ns;
-				opt_is_marginal = marginal;
 			}
 			break;
 		case NVME_ANA_NONOPTIMIZED:
-			if (is_best_distance(nonopt_is_marginal, marginal,
-					     min_depth_nonopt, depth)) {
+			if (depth < min_depth_nonopt) {
 				min_depth_nonopt = depth;
 				best_nonopt = ns;
-				nonopt_is_marginal = marginal;
 			}
 			break;
 		default:
 			break;
 		}
 
-		if (min_depth_opt == 0 && !opt_is_marginal)
+		if (min_depth_opt == 0)
 			return best_opt;
 	}
-
-	/*
-	 * Prefer non-marginal non-optimized path
-	 * over a marginal optimized path.
-	 */
-	if (opt_is_marginal && !nonopt_is_marginal && best_nonopt)
-		return best_nonopt;
 
 	return best_opt ? best_opt : best_nonopt;
 }
